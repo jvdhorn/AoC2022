8dd146fc95972e25bf4727adcb5c9258261141d1
Jitse van der Horn <jitsie@gmail.com>
2022-12-20 13:42:59 +0100

    Refactor

--- 05/solution.py
+++ 05/solution.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 
-sol = 2
+def parse_input(inp):
+
+  stacks  = [[*''.join(st).strip()[:-1]] for st in [*zip(*inp[:9])][1::4]]
+  actions = [[*map(int,ln.split()[1::2])] for ln in inp[10:]]
+
+  return stacks, actions
+
+
+def solution(stacks, actions, rev=True):
+
+  stacks = [stack[:] for stack in stacks]
+
+  for x, a, b in actions:
+    stacks[b-1][:0], stacks[a-1][:x] = stacks[a-1][:x][::1-rev*2], []
+  
+  return ''.join([*zip(*stacks)][0])
+
 
 if __name__ == '__main__':
   with open('input.txt') as inp:
     raw       = inp.read().splitlines()
-    part_1    = [[*''.join(st).strip()[:-1]] for st in [*zip(*raw[:9])][1::4]]
-    part_2    = [[*map(int,ln.split()[1::2])] for ln in raw[10:]]
-    for x, a, b in part_2:
-      part_1[b-1][:0], part_1[a-1][:x] = part_1[a-1][:x][::sol*2-3], []
-    solution = ''.join([*zip(*part_1)][0])
-    print(solution)
+    stck, act = parse_input(raw)
+    solution1 = solution(stck, act, True)
+    print(solution1)
+    solution2 = solution(stck, act, False)
+    print(solution2)
