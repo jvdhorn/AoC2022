8273882adb139c1ef23e5b14f1c770095e825522
Jitse van der Horn <jitsie@gmail.com>
2022-12-21 17:23:35 +0100

    Refactor

--- 21/solution.py
+++ 21/solution.py
@@ -29,7 +29,7 @@ def monkey_reduce(monkeys, stop=''):
 
     check_again = False
 
-    for monkey in list(monkeys):
+    for monkey in monkeys:
       if len(monkeys[monkey]) == 1 and monkey != stop:
         number      = monkeys.pop(monkey)[0]
         for val in monkeys.values():
